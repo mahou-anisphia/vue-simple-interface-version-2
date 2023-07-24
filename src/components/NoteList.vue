25798df03bacac247de7c3b9ec9d07701563a512
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-07-25 00:07:25 +0700

    init

---
+++ src/components/NoteList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-for="note in notes" v-bind:key="note">
+    <p>{{ note }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      notes: ["anisphia", "mahou", "is", "the", "best"],
+    };
+  },
+};
+</script>
