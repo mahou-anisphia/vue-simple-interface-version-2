42fc09b676e541e131ce8e66ba48ebff561be13e
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-07-30 00:07:17 +0700

    added nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <v-app>
-    <v-toolbar app> </v-toolbar>
+    <v-toolbar app>
+      <v-avatar rounded="0" size="x-large">
+        <v-img src="img/ehc-logo.png"></v-img>
+      </v-avatar>
+      <v-btn flat>Requests</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn flat>Login</v-btn>
+    </v-toolbar>
     <v-container>
       <v-main>
         <AddRequest />
